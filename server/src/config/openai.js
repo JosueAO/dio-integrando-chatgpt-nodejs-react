0602b44e2dd9c4730d4bf710a434abdc2386f5ca
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2023-12-08 17:55:31 -0300

    dependencias, configuracoes chatgpt, controllers, inicio fronted e css reset/padronizado

---
+++ server/src/config/openai.js
@@ -0,0 +1,24 @@
+const { Configuration, OpenAI } = require = ("openai");
+
+
+module.exports = class openai{
+    static configuration() {
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+          });
+
+          return new OpenAI(this.configuration)
+    }
+
+    static textCompletion({prompt}){
+        return{
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3500,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        }
+    }
+}
